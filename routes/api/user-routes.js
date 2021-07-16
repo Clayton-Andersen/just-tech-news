80573dacedd64f1d9fda0e1476868277cad1fb22
Aloha <utah.clayton@gmail.com>
2021-07-16 06:56:10 -0700

    Created User class in User.js and began adding api routes  in user-routes.js

---
+++ routes/api/user-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', (req, res) => {
+    // Access our User model and run .findAll() method
+    User.findAll()
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {});
+// 13.1.6 For the second GET route that only returns one user based on its req.params.id value, let's use a variation of the .findAll() method. Update the route to look like the following code:
+
+// POST /api/users
+router.post('/', (req, res) => {});
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {});
+
+module.exports = router;
