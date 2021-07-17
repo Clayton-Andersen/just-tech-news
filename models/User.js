098731bd6ef6625a8f572e3fbacc827027a2ace5
Aloha <utah.clayton@gmail.com>
2021-07-16 22:06:15 -0700

    hased passwords using bcrypt

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -8,23 +9,16 @@ class User extends Model { }
 User.init(
   {
     // TABLE COLUMN DEFINITIONS GO HERE
-    // define an id column
     id: {
-      // use the special Sequelize DataTypes object provide what type of data it is
       type: DataTypes.INTEGER,
-      // this is the equivalent of SQL's `NOT NULL` option
       allowNull: false,
-      // instruct that this is the Primary Key
       primaryKey: true,
-      // turn on auto increment
       autoIncrement: true
     },
-    // define a username column
     username: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    // define an email column
     email: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -35,17 +29,27 @@ User.init(
         isEmail: true
       }
     },
-    // define a password column
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        // this means the password must be at least four characters long
         len: [4]
       }
     }
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
     // pass in our imported sequelize connection (the direct connection to our database)
